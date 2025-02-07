db309bbf79424d9cf59b8de201f859669479899e
dellibabus <sdellibabu8@gmail.com>
2025-02-07 21:20:56 +0530

    `Update API URLs in ScreenPage.jsx to use production endpoint`

--- src/Pages/ScreenPage.jsx
+++ src/Pages/ScreenPage.jsx
@@ -14,7 +14,7 @@ const ScreensPage = () => {
   const fetchScreen = async () => {
     try {
       await axios
-        .get("http://localhost:7000/screen/getallscreen", {
+        .get("https://popcornspotbackend-production.up.railway.app/screen/getallscreen", {
           headers: { Authorization: `Bearer ${authToken}` },
         })
         .then((res) => {
@@ -33,7 +33,7 @@ const ScreensPage = () => {
   const handleDelete = async (_id) => {
     try {
       await axios
-        .delete(`http://localhost:7000/screen/delete/?_id=${_id}`, {
+        .delete(`https://popcornspotbackend-production.up.railway.app/screen/delete/?_id=${_id}`, {
           headers: { Authorization: `Bearer ${authToken}` },
         })
         .then(async (res) => {
